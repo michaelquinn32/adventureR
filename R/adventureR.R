3af3f4b971941dbfc9e172880df6774010eae435
michaelquinn32 <michael.quinn@aya.yale.edu>
2015-12-05 16:04:27 -0500

    Typos

--- R/adventureR.R
+++ R/adventureR.R
@@ -1,12 +1,14 @@
 #' adventureR: the big package of R adventures
 #'
 #' This package accompanies the \href{http://michaelquinn32.github.io/}{Big Blog of R Adventures}.
-#' Since it's only unifying theme is the blog, it's primary function is to gather, document and share
+#' Since its only unifying theme is the blog, its primary function is to gather, document and share
 #' the code developed in support of that blog. For that reason, the package should always be considered
 #' "in development."
 #'
 #' The code here will also be on the raw side. Instead of running perfectly under most use cases,
-#' I am much more interested in producing code that can be easily interpreted.
+#' I am much more interested in producing code that can be easily interpreted. Again, we're
+#' illustrating how R can solve lots of problems, not how to write the world's most perfect
+#' R code (even if there is such a thing).
 #'
 #' @docType package
 #' @name adventureR
